24718155b9462a327a05dde079f58224b2bfa627
k.brandon <kevin.brandonuk@outlook.com>
2019-08-13 14:05:19 +0100

    reduced the amount of validaiton information displayed

--- NCS.DSS.EmploymentProgression/PostEmploymentProgression/Function/EmploymentProgressionPostTrigger.cs
+++ NCS.DSS.EmploymentProgression/PostEmploymentProgression/Function/EmploymentProgressionPostTrigger.cs
@@ -29,15 +29,9 @@ namespace NCS.DSS.LearningProgression
         [Response(HttpStatusCode = (int)422, Description = "Employment progression validation error(s).", ShowSchema = false)]
         [Display(Name = "Post", Description = "Ability to create a new Employment Progression for a customer. <br>" +
                                               "<br> <b>Validation Rules:</b> <br>" +
-                                              "<br><b>DateProgressionRecorded:</b> ISO8601:2004.  <= datetime.now <br>" +
-                                              "<br><b>CurrentEmploymentStatus:</b> A valid EmploymentStatus from enum. <br>" +
-                                              "<br><b>EconomicShockStatus:</b> A valid EconomicShockStatus from enum. <br>" +
                                               "<br><b>EconomicShockCode:</b> Mandatory if EconomicShockStatus = 2 - Government defined economic shock. <br>" +
                                               "<br><b>EmploymentHours:</b> A valid EconomicShockStatus from enum. If CurrentEmployment status = 1, 4, 5, 8, 9 then the item must be a valid EmploymentHours reference data item<br>" +
-                                              "<br><b>DateOfEmployment:</b> If CurrentEmployment status = 1, 4, 5, 8, 9 then the item is mandatory, ISO8601:2004 <= datetime.now <br>" +
-                                              "<br><b>DateOfLastEmployment:</b> ISO8601:2004 <= datetime.now<br>" +
-                                              "<br><b>LengthOfUnemployment:</b> A valid LengthOfUnemployment from enum. <br>" +
-                                               "<br><b>LastModifiedDate:</b> ISO8601:2004 <br>"
+                                              "<br><b>DateOfEmployment:</b> If CurrentEmployment status = 1, 4, 5, 8, 9 then the item is mandatory, ISO8601:2004 <= datetime.now <br>"
                                                 )]
         [ProducesResponseType(typeof(EmploymentProgression.Models.EmploymentProgression), (int)HttpStatusCode.OK)]
         public async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = RouteValue)]HttpRequest req, ILogger logger, string customerId)
